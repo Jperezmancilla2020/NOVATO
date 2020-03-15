a4b5ce879aa9c544e68f7a015069821b78ca55e9
JavierPerez <javierperez0508@hotmail.com>
2020-03-14 18:49:54 -0600

    Version 1.2Version 1.2

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 texto <?php DOCTYPE ?>
+
+
+
+
+texto <!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
